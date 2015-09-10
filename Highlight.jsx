661de8af4d36eeec537cb76fc2d303c38be0f02b
Andrey <andreykvital@gmail.com>
2015-09-10 18:02:48 -0300

    Initial code

---
+++ Highlight.jsx
@@ -0,0 +1,13 @@
+/**
+ * @author Andrey K. Vital <andreykvital@gmail.com>
+ * @flow
+ */
+import React, {Component} from 'react'
+
+export default class Highlight extends Component {
+  render(): ReactElement {
+    return (
+      <span></span>
+    )
+  }
+}
