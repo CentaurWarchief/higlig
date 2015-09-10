9ba5fdb775db0a25aa1266dccbec305dbc2e7844
Andrey <andreykvital@gmail.com>
2015-09-10 18:15:54 -0300

    Add implementation

--- Highlight.jsx
+++ Highlight.jsx
@@ -5,9 +5,41 @@
 import React, {Component} from 'react'
 
 export default class Highlight extends Component {
+  props: {
+    word: string,
+    wrap: (word: string) => ReactElement | string,
+    children: string
+  };
+
+  static defaultProps: {};
+
   render(): ReactElement {
+    var {word, children, wrap} = this.props
+
+    var regex = new RegExp('('+ word +')', 'i')
+
+    var matches = children.match(regex)
+
+    if (! matches) {
+      return (
+        <span>{children}</span>
+      )
+    }
+
+    var segments = children.split(regex)
+
     return (
-      <span></span>
+      <span>
+        {segments.map((segment: string, index: number) => {
+          if (segment.trim() === matches[0]) {
+            return wrap(matches.shift())
+          }
+
+          return (
+            <span>{segment}</span>
+          )
+        })}
+      </span>
     )
   }
 }
